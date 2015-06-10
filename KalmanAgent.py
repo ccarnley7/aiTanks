86ddc79662c9e2931f13954d4000e650bcd5b325
Christian Carnley <ccarnley7@gmail.com>
2015-06-10 14:10:35 -0600

    added cleaner plotting

--- KalmanAgent.py
+++ KalmanAgent.py
@@ -59,8 +59,6 @@ class KalmanAgent(object):
         length_sqd = (math.pow(end[0] - start[0], 2) + math.pow(start[1] - end[1], 2))
         
         t = ((point[0] - start[0]) * (end[0] - start[0]) + (point[1] - start[1]) * (end[1] - start[1])) / length_sqd
-        # if t < 0: return self.getDistance(point, start)
-        # if t > 1: return self.getDistance(point, end)
         if t < 0 or t > 1: return 9001
         
         return self.getDistance(point, [start[0] + t * (end[0] - start[0]), start[1] + t * (end[1] - start[1])])
@@ -81,9 +79,8 @@ class KalmanAgent(object):
             self.goalPos = self.chooseTargetFlag()
         self.distance = math.sqrt((self.goalPos[0] - self.currentTank.x) ** 2 + (self.currentTank.y - self.goalPos[1]) ** 2)
         self.angle = math.atan2(self.goalPos[1] - self.currentTank.y, self.goalPos[0] - self.currentTank.x)
-        # normalize angle?
-        spread = 10  # not sure what this should be
-        alpha = 10  # not sure what this should be
+        spread = 10 
+        alpha = 10  
         
         if self.distance < self.goalRadius:
             self.dx = 0
@@ -91,12 +88,9 @@ class KalmanAgent(object):
         elif self.goalRadius <= self.distance and self.distance <= spread + self.goalRadius:
             self.dx = alpha * (self.distance - self.goalRadius) * math.cos(self.angle)
             self.dy = alpha * (self.distance - self.goalRadius) * math.sin(self.angle)
-        else:  # distance > spread+radius
+        else: 
             self.dx = alpha * spread * math.cos(self.angle)
             self.dy = alpha * spread * math.sin(self.angle)
-        
-        # print 'dx: ', self.dx
-        # print 'dy: ', self.dy
         
     def repulsiveFields(self, x=-1, y=-1):
         if x != -1 and y != -1:
@@ -106,7 +100,7 @@ class KalmanAgent(object):
         spread = 10;
         beta = 20;
         
-        if self.currentTank.flag != '-':  # they should get out of HIS way
+        if self.currentTank.flag != '-': 
             return
         
         for tank in self.mytanks:
@@ -115,7 +109,6 @@ class KalmanAgent(object):
             
             distance = math.sqrt(math.pow(tank.x - self.currentTank.x, 2) + math.pow(self.currentTank.y - tank.y, 2))
             angle = self.normalize_angle(math.atan2(tank.y - self.currentTank.y, tank.x - self.currentTank.x))
-            # normalize angle?
             
             if distance < radius:
                 doX = math.copysign(1, math.cos(angle)) * -9001
@@ -123,7 +116,7 @@ class KalmanAgent(object):
             elif radius <= distance and distance <= (spread + radius):
                 doX = -beta * (spread + radius - distance) * math.cos(angle)
                 doY = -beta * (spread + radius - distance) * math.sin(angle)    
-            else:  # distance > spread + radius
+            else:
                 doX = 0;
                 doY = 0;
                 
@@ -138,9 +131,7 @@ class KalmanAgent(object):
         spread = 10
         beta2 = 100
         for obstacle in self.obstacles:
-            # i= 0
             for i in range(0, 4):
-                # print 'obstacle[',i,']: ', obstacle[i]
                 start = obstacle[i]
                 if i == 3: end = obstacle[0]
                 else: end = obstacle[i + 1]
@@ -148,9 +139,7 @@ class KalmanAgent(object):
                 point = [self.currentTank.x, self.currentTank.y]
             
                 distance = self.distanceToLine(start, end, point)
-                # angle = self.normalize_angle(math.atan2(end[1] - start[1], end[0] - start[0]))  # - math.pi/2 ?
                 angle = (math.atan2(end[0] - start[0], -(end[1] - start[1]))) - math.pi / 2
-                # normalize?
             
                 if distance < radius:
                     print "It's over 9000! Tangential angle: ", angle
@@ -162,7 +151,7 @@ class KalmanAgent(object):
                     print "It's within the spread! Tangential angle: ", angle
                     doX = beta2 * (spread + radius - distance) * math.cos(angle)
                     doY = beta2 * (spread + radius - distance) * math.sin(angle)    
-                else:  # distance > spread + radius
+                else: 
                     doX = 0;
                     doY = 0;
                 
@@ -171,28 +160,16 @@ class KalmanAgent(object):
         
     def applyDelta(self):
         threshold = .5
-        # set up speed and angvel based on delta
         if self.skeetMode and not self.setup: self.curSpeed = 0
         else: self.curSpeed = math.sqrt(math.pow(self.dx, 2) + math.pow(self.dy, 2)) / 100
-        angle = math.atan2(self.dy, self.dx)  # angle we want to face
-        curAngle = self.currentTank.angle  # angle we are currently facing
-        # print 'current speed', self.curSpeed
-        # print 'target angle: ', angle
-        # print 'current angle: ', curAngle
-#         print 'dx: ', self.dx
-#         print 'dy: ', self.dy
+        angle = math.atan2(self.dy, self.dx)  
+        curAngle = self.currentTank.angle  
         
         angleDif = self.normalize_angle(angle - curAngle)
-        # angleDif= math.fabs(curAngle - angle) #does this need to be normalized?
-        # signedAngleDif= angle - curAngle
-        # print 'Angle difference: ', angleDif
-        if angleDif < threshold and angleDif > -threshold:  # this is a shot in the dark. Tuning needed
-            # self.curAngVel= signedAngleDif
+        if angleDif < threshold and angleDif > -threshold: 
             self.curAngVel = angleDif * 2
         else:
-            # self.curAngVel= math.copysign(1, signedAngleDif) #should come out to 1 or -1  
             self.curAngVel = math.copysign(1, angleDif)
-        # print 'angvel: ', self.curAngVel 
         
     def chooseTargetFlag(self):
         bestDistance = 9999
@@ -242,7 +219,7 @@ class KalmanAgent(object):
         
         distFlag = self.getDistance([curTank.x, curTank.y], [flagX, flagY])
         
-        if distFlag < self.flagZone or self.skeetMode:  # defend the flag from enemies!
+        if distFlag < self.flagZone or self.skeetMode:
             targetEnemy = self.chooseTargetEnemy()
             if targetEnemy is not None:
                 return targetEnemy
@@ -250,11 +227,10 @@ class KalmanAgent(object):
         return self.chooseTargetFlag() 
     
     def initMatrices(self):
-        dt = .01  # delta t. Approximately .01 seconds pass between ticks
-#         c = -.0001 # this may end up being zero
+        dt = .01 
         c = 0
         dt2 = math.pow(dt, 2) / 2
-        sd = 25  # standard deviation of the noise squared
+        sd = 25 
         pos = .1
         vel = .2
         acc = 5
@@ -270,22 +246,18 @@ class KalmanAgent(object):
         self.FT = self.F.T
         self.HT = self.H.T
         
-#         print 'Matrix F', self.F
-        
     def resetConfidence(self):
-        print '\n\n------------- Resetting confidence --------------\n\n'
         pos = 5
-        vel = .1  # velocity
-        acc = .1  # acceleration
+        vel = .1  
+        acc = .1 
         self.sig_t = np.matrix([[pos, 0, 0, 0, 0, 0], [0, vel, 0, 0, 0, 0], [0, 0, acc, 0, 0, 0], [0, 0, 0, pos, 0, 0], [0, 0, 0, 0, vel, 0], [0, 0, 0, 0, 0, acc]])    
         self.Mu = np.matrix([[self.zt1.tolist()[0][0]], [1], [0], [self.zt1.tolist()[1][0]], [1], [0]])
         
-    def updateK(self, A):  # A is F*sig_t*FT + sigX, but we save time by computing once, since it's used elsewhere too
+    def updateK(self, A): 
         self.K = (A * self.HT) * (self.H * A * self.HT + self.sigZ).I 
         
     def updateMu(self):
         self.Mu = (self.F * self.Mu) + (self.K * (self.zt1 - (self.H * self.F * self.Mu)))
-#         print 'estimated velocity:', self.Mu[1][0] + self.Mu[4][0]
         
     def updateSig_t(self, A):
         I = np.matrix(np.identity(6))
@@ -298,20 +270,12 @@ class KalmanAgent(object):
             sig_y = math.sqrt(sig2_y)
             rhoSigxSigy = self.sig_t[0, 3]
             rho = rhoSigxSigy / (sig_x * sig_y)
-            print sig_x
-            print sig_y
-            print rho
-            
-            
-            # kalmanPlotting = KalmanPlotting.KalmanPlotting(sig2_x, sig2_y, rho, self.plotId)
-            # self.plotId += 1
-            # self.plotTimer = random.randrange(1, 6)
         
     def estimateMu(self):    
         return self.F * self.Mu 
     
     def predictMu(self, future):
-        ticks = int(future / .01)  # ticks from now = seconds from now / (seconds per tick) 
+        ticks = int(future / .01)
         estimate = self.estimateMu()
         for tick in range(1, ticks + 1):
             estimate = self.F * estimate
@@ -321,7 +285,6 @@ class KalmanAgent(object):
     def tick(self, gameClock):   
         """Some time has passed; decide what to do next."""
         self.tickTime = gameClock - self.prevTime
-#         print 'tickTime:', self.tickTime 
         resetInterval = 10
         mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
         self.obstacles = self.bzrc.get_obstacles()
@@ -348,15 +311,6 @@ class KalmanAgent(object):
             self.timer = resetInterval
             self.resetConfidence()
         
-#         self.gunTimer -= self.tickTime
-#         # self.moveTimer -= self.tickTime
-#         if self.gunTimer <= 0:
-#             shouldShoot = True
-#             self.gunTimer = float(random.randrange(15, 25)) / 10
-#             # print 'gunTimer reset: ', self.gunTimer
-#         else:
-#             shouldShoot = False
-#         if len(shots) != 0: print shots[0].vx, shots[0].vy
         shouldShoot = True
         shotSpeed = 100
 
@@ -364,86 +318,40 @@ class KalmanAgent(object):
             if flags[index].color == self.constants['team']:
                 self.teamIndex = index
         
-        # test with one tank
-        if self.debug:
-            self.currentTank = mytanks[0]
-        
-            if self.currentTank.flag != '-':
-                self.goalPos = self.homeBase
-            else:   
-                goalReading = self.chooseTarget() 
-                self.zt1 = np.matrix(goalReading).T
-#                 print 'last Mu:', self.Mu
-                estimate = (self.H * self.estimateMu()).T.tolist()[0]
-                dist = self.getDistance([self.currentTank.x, self.currentTank.y], estimate)
-                if self.oldDist == 0: self.oldDist = dist
-                distDiff = dist - self.oldDist
-                future = (dist + distDiff) / shotSpeed
-                self.oldDist = dist
-#                 print 'Distance:', dist, 'future:', future
-#                 futurePos = self.predictMu(future)
-                self.goalPos = self.predictMu(future)
+        self.currentTank = mytanks[0]
+    
+        if self.currentTank.flag != '-':
+            self.goalPos = self.homeBase
+        else:   
+            goalReading = self.chooseTarget() 
+            self.zt1 = np.matrix(goalReading).T
+            estimate = (self.H * self.estimateMu()).T.tolist()[0]
+            dist = self.getDistance([self.currentTank.x, self.currentTank.y], estimate)
+            if self.oldDist == 0: self.oldDist = dist
+            distDiff = dist - self.oldDist
+            future = (dist + distDiff) / shotSpeed
+            self.oldDist = dist
+            self.goalPos = self.predictMu(future)
+            A = (self.F * self.sig_t * self.FT) + self.sigX
+            self.updateK(A)
+            self.updateMu()
+            self.updateSig_t(A)
+            self.estimatesX.append(estimate[0])
+            self.estimatesY.append(estimate[1])
+            self.obsX.append(self.enemies[0].x)
+            self.obsY.append(self.enemies[0].y)
 
-                
-#                 dist2 = self.getDistance([self.currentTank.x, self.currentTank.y], futurePos)
-#                 timeDiff = (dist2 - dist) / shotSpeed
-#                 ticksDiff = int(timeDiff / .01)
-#                 goalX = futurePos[0] + (self.estimateMu()[1][0] * abs(ticksDiff))
-#                 goalY = futurePos[1] + (self.estimateMu()[4][0] * abs(ticksDiff))
-#                 self.goalPos = [goalX, goalY]
-#                 print 'Future position', futurePos, 'Aiming at:', self.goalPos
-#                 print 'Lead if same distance:', future, 'Lead based on predicted distance', adjustedFuture
-#                 self.goalPos = (self.H * self.estimateMu()).T.tolist()[0]
-                
-                A = (self.F * self.sig_t * self.FT) + self.sigX
-                self.updateK(A)
-                self.updateMu()
-                self.updateSig_t(A)
-                self.estimatesX.append(estimate[0])
-                self.estimatesY.append(estimate[1])
-                self.obsX.append(self.enemies[0].x)
-                self.obsY.append(self.enemies[0].y)
-                plt.plot(estimate[0], estimate[1], 'bo', self.enemies[0].x, self.enemies[0].y, 'ro')
-                plt.draw()
-                #plt.plot([self.enemies[0].x], [self.enemies[0].y], 'ro')
-                
-                
-
-#             print 'goal:', self.goalPos, 'reading:', goalReading    
+    
+        self.attractiveField()
+        self.repulsiveFields()
+        self.tangentialFields()
+        self.applyDelta()
+        if self.curSpeed == 0 and self.enemies[0].x > -600:
+            plt.plot(estimate[0], estimate[1], 'bo', self.enemies[0].x, self.enemies[0].y, 'ro')
+            plt.draw()
+        command = Command(self.currentTank.index, self.curSpeed, self.curAngVel, shouldShoot)
+        self.commands.append(command)
         
-            self.attractiveField()
-            self.repulsiveFields()
-            self.tangentialFields()
-            self.applyDelta()
-            # if self.curSpeed == 0: 
-            # 	plt.show()
-            command = Command(self.currentTank.index, self.curSpeed, self.curAngVel, shouldShoot)
-            self.commands.append(command)
-        
-        else:
-            # try more tanks after one works
-            i = 0     
-            for tank in mytanks:
-                if tank.status == 'dead':
-                    continue
-                self.currentTank = tank
-                
-                if self.currentTank.flag != '-':
-                    self.goalPos = self.homeBase
-                else:
-                    self.goalPos = self.chooseTarget()
-            
-                """apply fields to set up command"""
-#                 if self.prevTime == 0.0 and i == 0:
-#                     self.fields = fields.Fields(self)
-                self.attractiveField()
-                self.repulsiveFields()
-                self.tangentialFields()
-                self.applyDelta()
-            
-                command = Command(tank.index, self.curSpeed, self.curAngVel, shouldShoot)
-                self.commands.append(command)
-                i += 1
             
         self.bzrc.do_commands(self.commands)
         self.prevTime = gameClock
